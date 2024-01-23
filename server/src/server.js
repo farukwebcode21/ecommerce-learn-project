eec2b75f0d15fc4227b658ad27d03aab829f6918
farukwebcode21 <farukahmadmk@yahoo.com>
2024-01-23 23:42:11 +0600

    testing client errror and server error

--- server/src/server.js
+++ server/src/server.js
@@ -37,6 +37,18 @@ app.get("/api/user", isLoggedIn, (req, res) => {
   });
 });
 
+// client error handling
+app.use((req, res, next) => {
+  res.status(404).json({ message: "route not fount" });
+  next();
+});
+
+// server error handling
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
+
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
