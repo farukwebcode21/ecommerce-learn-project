22b21f2562b8d88a943d28ecbaf41a13a31597c4
farukwebcode21 <farukahmadmk@yahoo.com>
2024-01-25 22:43:01 +0600

    connect mongodb and seed data database

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -5,9 +5,6 @@ const connectDatabase = async (option = {}) => {
   try {
     await mongoose.connect(mongodbURL, option);
     console.log("connection to mongodb successfully");
-    mongoose.connection.on("error", (error) => {
-      console.log("DB connection error:", error);
-    });
   } catch (error) {
     console.log("Could not connect database", error.toString());
   }
--- server/src/secret.js
+++ server/src/secret.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
+
 const serverPort = process.env.SERVER_PORT || 5001;
+
 const mongodbURL =
   process.MONGODB_ATLAS || "mongodb://localhost:27017/ecommerceMern";
 
